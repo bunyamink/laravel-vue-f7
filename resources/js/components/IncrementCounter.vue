ace2feac331326e056ab08672ec8b756fe32d920
bunyamink <bunyaminibrahim@gmail.com>
2023-12-24 12:04:49 +0300

    first commit

---
+++ resources/js/components/IncrementCounter.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+ 
+const counter = ref(0)
+</script>
+ 
+<template>
+  <button
+    type="button"
+    @click="counter++"
+    style="color: white; font-size: 20px;"
+  >
+    Counter is: {{ counter }}
+  </button>
+</template>
