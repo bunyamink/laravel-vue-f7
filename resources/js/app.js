28d8859c39f6ec34f917716eb3cd7a557d4fe419
bunyamink <bunyaminibrahim@gmail.com>
2023-12-25 22:44:03 +0300

    framework7 lite version activated

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,34 @@
 import { createApp } from 'vue';
-import IncrementCounter from './components/IncrementCounter.vue';
 
 // Import Framework7 Core
-//import Framework7 from 'framework7/lite';
+import Framework7 from 'framework7/lite';
 
 // Import Framework7 Vue
-//import Framework7Vue from 'framework7-vue';
+import Framework7Vue from 'framework7-vue';
+
+// Import Framework7
+//import Input from 'framework7/components/input';
+//import Dialog from 'framework7/components/dialog';
+//import ColorPicker from 'framework7/components/color-picker';
+//import Popover from 'framework7/components/popover';
+//import Range from 'framework7/components/range';
+//import Toggle from 'framework7/components/toggle';
+import Popup from 'framework7/components/popup';
+import Panel from 'framework7/components/panel';
+//import Tooltip from 'framework7/components/tooltip';
 
 // Import F7 Bundle
-import Framework7 from 'framework7/lite-bundle'
+//import Framework7 from 'framework7/lite-bundle'
 
 // Import F7-Vue Plugin Bundle (with all F7 components registered)
-import Framework7Vue, { registerComponents } from 'framework7-vue/bundle'
+//import Framework7Vue, { registerComponents } from 'framework7-vue/bundle'
 
-Framework7.use(Framework7Vue)
+Framework7.use(Framework7Vue);
+Framework7.use([
+    Popup,
+    Panel,
+    Popover
+]);
 
 // Import Main App component
 import App from './components/app.vue';
@@ -22,11 +37,7 @@ import App from './components/app.vue';
 const app = createApp(App);
 
 // Register all Framework7 Vue components
-registerComponents(app);
+// registerComponents(app);
 
 // Mounte Vue App
 app.mount('#app');
- 
-//createApp({}).mount('#app')
-
-// Init plugin
--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,7 +1,6 @@
-
 <template>
     <!-- Main Framework7 App component where we pass Framework7 params -->
-    <f7-app v-bind="{ theme: 'auto', name: 'My App' }">
+    <f7-app v-bind="f7params">
   
       <!-- Left Panel with "cover" effect -->
       <f7-panel left cover>
@@ -31,21 +30,15 @@
       <f7-view main>
         <f7-page>
           <f7-navbar title="Awesome App"></f7-navbar>
-          <!-- Page content -->
-          <f7-block>
-            <p>Here comes main view page text</p>
-          </f7-block>
-          <!-- Buttons to open panels -->
-          <f7-row>
-            <f7-col>
-              <f7-button panel-open="left">Left Panel</f7-button>
-            </f7-col>
-            <f7-col>
-              <f7-button panel-open="right">Right Panel</f7-button>
-            </f7-col>
-          </f7-row>
-          <!-- Button to open popup -->
-          <f7-button popup-open="#my-popup">Open Popup</f7-button>
+            <!-- Page content -->
+            <f7-block>
+              <p>Here comes main view page text</p>
+            </f7-block>
+            <!-- Buttons to open panels -->
+            <f7-button panel-open="left">Left Panel</f7-button>
+            <f7-button panel-open="right">Right Panel</f7-button>
+            <!-- Button to open popup -->
+            <f7-button popup-open="#my-popup">Open Popup</f7-button>
         </f7-page>
       </f7-view>
   
@@ -67,3 +60,31 @@
       </f7-popup>
     </f7-app>
   </template>
+
+<script>
+import { f7, f7App, f7View, f7Page, f7Navbar, f7Block,  f7Button, f7Popup, f7NavRight, f7Link, f7Panel } from 'framework7-vue';
+
+export default {
+  components: {
+    f7,
+    f7App,
+    f7View,
+    f7Page,
+    f7Navbar,
+    f7Block,
+    f7Button,
+    f7Popup,
+    f7NavRight,
+    f7Link,
+    f7Panel,
+  },
+  data(){
+    return {
+      f7params: { 
+        theme: 'auto', 
+        name: 'Laravel Vue F7',
+      }
+    }
+  }
+}
+</script>
